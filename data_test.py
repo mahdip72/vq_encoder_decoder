ddd1c524aaa704466a94d62c7eafeefd1020c00b
Farzaneh Esmaili <farzanehesmaili185@gmail.com>
2024-04-11 17:10:17 -0500

    Add vqva test with fashion mnist

--- data_test.py
+++ data_test.py
@@ -3,6 +3,9 @@ from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import make_blobs
 import numpy as np
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 
 
 class CustomDataset(Dataset):
@@ -23,31 +26,48 @@ def generate_data(n_samples, n_features, centers, cluster_std):
     return X, y
 
 
+def load_fashion_mnist_data(batch_size, shuffle):
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+    )
+    dataset = datasets.FashionMNIST(
+        root="~/data/fashion_mnist", train=True, download=True, transform=transform
+    )
+    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+    return data_loader
+
 def main():
     # Set a fixed random seed for reproducibility
-    np.random.seed(42)
+    # np.random.seed(42)
+    #
+    # # Parameters for dataset generation
+    # n_samples = 10000
+    # n_features = 8
+    # centers = 16
+    # cluster_std = 1.2
+    # test_size = 0.2
+    # batch_size = 64
+    #
+    # # Generate data
+    # X, y = generate_data(n_samples, n_features, centers, cluster_std)
+    #
+    # # Prepare datasets
+    # x_train, x_valid, y_train, y_valid = train_test_split(X, y, test_size=test_size, random_state=42)
+    # train_dataset = CustomDataset(x_train, y_train)
+    # valid_dataset = CustomDataset(x_valid, y_valid)
+    #
+    # # Prepare DataLoaders
+    # train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    # valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
+    #
+    # return train_loader, valid_loader
+    batch_size = 256
+    shuffle = True
 
-    # Parameters for dataset generation
-    n_samples = 10000
-    n_features = 8
-    centers = 16
-    cluster_std = 1.2
-    test_size = 0.2
-    batch_size = 64
+    # Load FashionMNIST data
+    train_loader = load_fashion_mnist_data(batch_size, shuffle)
 
-    # Generate data
-    X, y = generate_data(n_samples, n_features, centers, cluster_std)
-
-    # Prepare datasets
-    x_train, x_valid, y_train, y_valid = train_test_split(X, y, test_size=test_size, random_state=42)
-    train_dataset = CustomDataset(x_train, y_train)
-    valid_dataset = CustomDataset(x_valid, y_valid)
-
-    # Prepare DataLoaders
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
-
-    return train_loader, valid_loader
+    return train_loader
 
 
 if __name__ == "__main__":
--- train.py
+++ train.py
@@ -1,42 +1,69 @@
 import argparse
 import yaml
 import torch
+from tqdm.auto import trange
+import  load_configs, prepare_saving_dir
 import numpy as np
-from utils import load_configs, prepare_saving_dir
+from utils  import *
+from torch.utils.data import DataLoader
+from data_test import *
+from model import SimpleVQAutoEncoder
 
 
-def train_loop():
-    pass
+device = "cuda" if torch.cuda.is_available() else "cpu"
+num_codes = 256
+
+def train_loop(model, train_loader, train_iterations=1000, alpha=10):
+    def iterate_dataset(data_loader):
+        data_iter = iter(data_loader)
+        while True:
+            try:
+                x, y = next(data_iter)
+            except StopIteration:
+                data_iter = iter(data_loader)
+                x, y = next(data_iter)
+            yield x.to(device), y.to(device)
+
+    for _ in (pbar := trange(train_iterations)):
+        opt.zero_grad()
+        x, _ = next(iterate_dataset(train_loader))
+        out, indices, cmt_loss = model(x)
+        rec_loss = (out - x).abs().mean()
+        (rec_loss + alpha * cmt_loss).backward()
+
+        opt.step()
+        pbar.set_description(
+            f"rec loss: {rec_loss.item():.3f} | "
+            + f"cmt loss: {cmt_loss.item():.3f} | "
+            + f"active %: {indices.unique().numel() / num_codes * 100:.3f}"
+        )
+    return
+
 
 
 def evaluate_loop():
     pass
 
 
-def main(dict_config, config_file_path):
-    configs = load_configs(dict_config)
-
-    if isinstance(configs.fix_seed, int):
-        torch.manual_seed(configs.fix_seed)
-        torch.random.manual_seed(configs.fix_seed)
-        np.random.seed(configs.fix_seed)
-
-    result_path, checkpoint_path = prepare_saving_dir(configs, config_file_path)
-
-    for epoch in range(1, configs.train_settings.num_epochs + 1):
-        train_loop()
-        evaluate_loop()
-
-    print("Train finished!")
-
-
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Train a deep neural nets.")
-    parser.add_argument("--config_path", "-c", help="The location of config file", default='./config.yaml')
-    args = parser.parse_args()
-    config_path = args.config_path
+    # parser = argparse.ArgumentParser(description="Train a deep neural nets.")
+    # parser.add_argument("--config_path", "-c", help="The location of config file", default='./config.yaml')
+    # args = parser.parse_args()
+    # config_path = args.config_path
+    #
+    # with open(config_path) as file:
+    #     config_file = yaml.full_load(file)
 
-    with open(config_path) as file:
-        config_file = yaml.full_load(file)
+    train_data = load_fashion_mnist_data(batch_size=256, shuffle=True)
 
-    main(config_file, config_path)
+    # result_path, checkpoint_path = prepare_saving_dir(configs, config_file_path)
+    lr = 3e-4
+    epoch = 1000
+    num_codes = 256
+    seed = 1234
+
+    torch.random.manual_seed(seed)
+    model = SimpleVQAutoEncoder(codebook_size=num_codes).to(device)
+    opt = torch.optim.AdamW(model.parameters(), lr=lr)
+    train_loop(model, train_data, train_iterations=epoch)
+    print("Train finished!")
