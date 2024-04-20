3cb1cf674d123e97bb2eff325733029c01426e35
mahdip72 <mahdipoormirzaie@gmail.com>
2024-04-19 23:55:52 -0500

    feat: use uv package installer instead of pip

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+pip install uv
 conda install --yes pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
 pip install torchmetrics
 conda install --yes h5py
 conda install --yes matplotlib
 conda install --yes pandas
-pip install accelerate
-pip install transformers
-pip install timm
-pip install 'git+https://github.com/katsura-jp/pytorch-cosine-annealing-with-warmup'
-pip install tensorboard
-pip install python-box
-pip install peft
-pip install -U scikit-learn
-pip install rdkit-pypi
-pip install vector-quantize-pytorch
+uv pip install accelerate
+uv pip install transformers
+uv pip install timm
+uv pip install 'git+https://github.com/katsura-jp/pytorch-cosine-annealing-with-warmup'
+uv pip install tensorboard
+uv pip install python-box
+uv pip install peft
+uv pip install -U scikit-learn
+uv pip install rdkit-pypi
+uv pip install vector-quantize-pytorch
