00b78df9a55c56f7abe4370819d0982051abfbeb
Farzaneh Esmaili <farzanehesmaili185@gmail.com>
2024-04-21 20:05:55 -0500

    fix inference

--- inference.py
+++ inference.py
@@ -16,31 +16,40 @@ def load_model(checkpoint_path='epoch_8.pth'):
     return model
 
 
-
-
-
-
 def main():
-    net = SimpleVQAutoEncoder(codebook_size=256)
+    import yaml
+    from utils import load_configs
+
+    config_path = "./config.yaml"
+
+    with open(config_path) as file:
+        config_file = yaml.full_load(file)
+
+    configs = load_configs(config_file)
+
+    net = SimpleVQAutoEncoder(
+        dim=configs.model.vector_quantization.dim,
+        codebook_size=configs.model.vector_quantization.codebook_size,
+        decay=configs.model.vector_quantization.decay,
+        commitment_weight=configs.model.vector_quantization.commitment_weight
+    )
 
     test_dataloader = load_fashion_mnist_data(batch_size=1, shuffle=False)
 
     with torch.inference_mode():
         for inputs in test_dataloader:
-                img_before = inputs[0].squeeze().numpy()
-                img_before = cv2.resize(img_before, (256, 256))
-                cv2.imshow('image', img_before)
-                cv2.waitKey(0)
+            img_before = inputs[0].squeeze().numpy()
+            img_before = cv2.resize(img_before, (256, 256))
+            cv2.imshow('image', img_before)
+            cv2.waitKey(0)
 
-                vq_output, indices, commit_loss = net(inputs[0], return_vq_only=True)
+            vq_output, indices, commit_loss = net(inputs[0], return_vq_only=True)
 
-                img_after = vq_output[0, 0]  # Shape (7, 7)
-                img_after = (img_after * 255).to(torch.uint8).numpy()
-                img_after = cv2.resize(img_after, (256, 256), interpolation=cv2.INTER_NEAREST)
-                cv2.imshow("Image", img_after)
-                cv2.waitKey(0)
-
-
+            img_after = vq_output[0, 0]  # Shape (7, 7)
+            img_after = (img_after * 255).to(torch.uint8).numpy()
+            img_after = cv2.resize(img_after, (256, 256), interpolation=cv2.INTER_NEAREST)
+            cv2.imshow("Image", img_after)
+            cv2.waitKey(0)
 
 
 if __name__ == '__main__':
