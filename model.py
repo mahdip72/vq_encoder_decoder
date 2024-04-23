5afde7d74370d96934aaf291822021bbace4dab6
mahdip72 <mahdipoormirzaie@gmail.com>
2024-04-22 20:04:17 -0500

    ref: prepare models function

--- model.py
+++ model.py
@@ -91,6 +91,9 @@ class SimpleVQAutoEncoder(nn.Module):
             nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(32),
             nn.GELU(),
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.GELU(),
             nn.MaxPool2d(kernel_size=2, stride=2)
         ])
         self.vq_layer = VectorQuantize(
@@ -102,6 +105,9 @@ class SimpleVQAutoEncoder(nn.Module):
         )
         self.decoder_layers = nn.ModuleList([
             nn.Upsample(scale_factor=2, mode="nearest"),
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.GELU(),
             nn.Conv2d(32, 16, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(16),
             nn.GELU(),
@@ -130,6 +136,56 @@ class SimpleVQAutoEncoder(nn.Module):
         return x.clamp(-1, 1), indices, commit_loss
 
 
+def get_nb_trainable_parameters(model):
+    r"""
+    Returns the number of trainable parameters and number of all parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        num_params = param.numel()
+        # if using DS Zero 3 and the weights are initialized empty
+        if num_params == 0 and hasattr(param, "ds_numel"):
+            num_params = param.ds_numel
+
+        # Due to the design of 4bit linear layers from bitsandbytes
+        # one needs to multiply the number of parameters by 2 to get
+        # the correct number of parameters
+        if param.__class__.__name__ == "Params4bit":
+            num_params = num_params * 2
+
+        all_param += num_params
+        if param.requires_grad:
+            trainable_params += num_params
+
+    return trainable_params, all_param
+
+
+def print_trainable_parameters(model, logging, description=""):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params, all_param = get_nb_trainable_parameters(model)
+    logging.info(
+        f"{description} trainable params: {trainable_params: ,} || all params: {all_param: ,} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+
+def prepare_models(configs, logging, accelerator):
+
+    model = SimpleVQAutoEncoder(
+        dim=configs.model.vector_quantization.dim,
+        codebook_size=configs.model.vector_quantization.codebook_size,
+        decay=configs.model.vector_quantization.decay,
+        commitment_weight=configs.model.vector_quantization.commitment_weight
+    )
+
+    if accelerator.is_main_process:
+        print_trainable_parameters(model, logging, 'VQ-VAE')
+
+    return model
+
+
 if __name__ == '__main__':
     import yaml
     from utils import load_configs
@@ -139,13 +195,13 @@ if __name__ == '__main__':
     with open(config_path) as file:
         config_file = yaml.full_load(file)
 
-    configs = load_configs(config_file)
+    main_configs = load_configs(config_file)
 
     net = TransformersVQAutoEncoder(
-        dim=configs.model.vector_quantization.dim,
-        codebook_size=configs.model.vector_quantization.codebook_size,
-        decay=configs.model.vector_quantization.decay,
-        commitment_weight=configs.model.vector_quantization.commitment_weight
+        dim=main_configs.model.vector_quantization.dim,
+        codebook_size=main_configs.model.vector_quantization.codebook_size,
+        decay=main_configs.model.vector_quantization.decay,
+        commitment_weight=main_configs.model.vector_quantization.commitment_weight
     )
 
     # create a random input tensor and pass it through the network
