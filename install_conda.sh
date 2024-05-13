60b518b87b4330cec789258367e93911b1fbd3d9
mahdi <mahdipoormirzaie@gmail.com>
2024-05-12 20:05:35 -0500

    fix: project dependencies
    Add and test the correct version of conda dependencies.

--- install_conda.sh
+++ install_conda.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-pip install uv
-conda install --yes pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
+conda install --yes pytorch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 pytorch-cuda=12.1 -c pytorch -c nvidia
+conda install --yes pyg -c pyg
 pip install torchmetrics
-conda install --yes h5py
-conda install --yes matplotlib
+pip install accelerate
+pip install transformers
+pip install timm
+pip install 'git+https://github.com/katsura-jp/pytorch-cosine-annealing-with-warmup'
+pip install tensorboard
+pip install python-box
+pip install fair-esm
 conda install --yes pandas
-uv pip install accelerate
-uv pip install transformers
-uv pip install timm
-uv pip install 'git+https://github.com/katsura-jp/pytorch-cosine-annealing-with-warmup'
-uv pip install tensorboard
-uv pip install python-box
-uv pip install peft
-uv pip install -U scikit-learn
-uv pip install rdkit-pypi
-uv pip install vector-quantize-pytorch
+pip install peft
+conda install --yes pytorch-scatter -c pyg
+conda install --yes pytorch-cluster -c pyg
+pip install h5py
+conda install --yes matplotlib
+conda install --yes openpyxl
