89fe4e33d1f06db7af62901354894770ee6fd2b1
Jarett Ren <zidar2003@gmail.com>
2024-05-24 17:02:24 -0500

    feat: started basic train loop for CIFAR

---
+++ train_cifar.py
@@ -0,0 +1,27 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from model.vqvae_model import VQVAE
+
+
+def train(model, criterion, train_loader, optimizer, epochs):
+    for epoch in range(epochs):
+        for i, (images, labels) in enumerate(train_loader):
+            optimizer.zero_grad()
+            output = model(images)
+            loss = criterion(output, labels)
+            loss.backward()
+            optimizer.step()
+
+
+if __name__ == "__main__":
+
+    # Normalize data to be in the range [-1.0, 1.0]
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    # Load CIFAR dataset
+    trainset = torchvision.datasets.CIFAR10(root='../data', train=True, download=True, transform=transform)
+    trainloader = DataLoader(trainset, batch_size=1, shuffle=True, num_workers=2)
