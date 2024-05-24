2b925e5b219bad0d5840c502b87fe33648abf53a
Jarett Ren <zidar2003@gmail.com>
2024-05-24 15:47:06 -0500

    feat: added decoder layers to vqvae_model.py and tested to confirm output size is 3x32x32

--- model/vqvae_model.py
+++ model/vqvae_model.py
@@ -1,12 +1,8 @@
-# 32x32x3 tensors
-# Layers: CNN, batch normalization, VQ
-
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
-from vector_quantize_pytorch import VectorQuantize, LFQ
+from vector_quantize_pytorch import VectorQuantize
 
 
 class VQVAE(nn.Module):
@@ -17,12 +13,12 @@ class VQVAE(nn.Module):
         super().__init__()
         self.d_model = kwargs['dim']
 
-        self.encoder_layers = nn.ModuleList([
+        self.encoder_layers = nn.Sequential(
             nn.Conv2d(3, self.d_model, kernel_size=3, stride=1, padding=1),
-            nn.MaxPool2d(kernel_size=2, stride=2),
             nn.BatchNorm2d(self.d_model),
-            nn.GELU()
-        ])
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
 
         self.vq_layer = VectorQuantize(
             dim=self.d_model,
@@ -32,30 +28,40 @@ class VQVAE(nn.Module):
             accept_image_fmap=True
         )
 
-        self.decoder_layers = nn.ModuleList([
-            nn.Conv2d(self.d_model, self.d_model, kernel_size=3, stride=1, padding=1)
-        ])
+        self.decoder_layers = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode='nearest'),
+            nn.Conv2d(self.d_model, 3, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(3),
+            nn.ReLU(),
+            nn.Tanh()
+        )
 
 
     def forward(self, x):
-        for layer in self.encoder_layers:
-            x = layer(x)
+        x = self.encoder_layers(x)
         x, indices, commit_loss = self.vq_layer(x)
+        x = self.decoder_layers(x)
         return x, indices, commit_loss
 
 
 if __name__ == "__main__":
 
+    # Normalize data to be in the range [-1.0, 1.0]
     transform = transforms.Compose(
         [transforms.ToTensor(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    # Load CIFAR dataset
+    testset = torchvision.datasets.CIFAR10(root='../../data', train=False, download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=1, shuffle=True, num_workers=2)
 
-    model = VQVAE(dim=1, codebook_size=32, decay=0.9, commitment_weight=0.9, accept_image_fmap=True)
+    # Ensure model gets and returns 3x32x32 tensors
+    model = VQVAE(dim=16, codebook_size=32, decay=0.9, commitment_weight=0.9, accept_image_fmap=True)
     for data in testloader:
         images, labels = data
-        quantized, indices, commit_loss = model(images)
-        print(indices)
-        break
+        print(images[0].size())
+        x, indices, commit_loss = model(images)
+        print(x[0].shape)
+
+        assert images[0].size() == torch.Size([3,32,32])
+        assert x[0].size() == torch.Size([3,32,32])
