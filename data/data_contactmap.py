d45d82b8dfe31497b239528e47d07bab9d9fc2ca
Jarett Ren <zidar2003@gmail.com>
2024-05-24 11:48:57 -0500

    feat: added new pdb_to_cmap function using the Biopython library (based on Duolin's code)

--- data/data_contactmap.py
+++ data/data_contactmap.py
@@ -1,26 +1,32 @@
-import pcmap
-import pypstruct
 import numpy as np
+from scipy.spatial import distance
 import matplotlib.pyplot as plt
 from pathlib import Path
 import torch
 from torch.utils.data import DataLoader, Dataset
+
+import pcmap
+import pypstruct
+import Bio.PDB
 
 
 class ContactMapDataset(Dataset):
     """
     Dataset for protein contact map
     """
-    def __init__(self, pdb_dir, transform=None):
+    def __init__(self, pdb_dir, threshold=8, chain="A", transform=None):
         self.pdbs = list(Path(pdb_dir).glob("*.pdb"))
+        self.threshold = threshold
+        self.chain = chain
 
     def __len__(self):
         return len(self.pdbs)
 
     def __getitem__(self, idx):
-        pdf_file = str(self.pdbs[idx])
-        contactmap = pdb_to_cmap(pdf_file)
-        return contactmap, pdf_file
+        pdb_file = str(self.pdbs[idx])
+        #contactmap = pdb_to_cmap_old(pdb_file) # Use the old function
+        contactmap = pdb_to_cmap(str(pdb_file), pdb_file, self.threshold, self.chain)
+        return contactmap, pdb_file
 
 
 def prepare_dataloaders(pdb_dir):
@@ -34,6 +40,8 @@ def prepare_dataloaders(pdb_dir):
     return data_loader
 
 
+###########################################################
+### OLD PDB_TO_CMAP FUNCTION (SLOW) ###
 def get_max_residue(pdb_file):
     """
     Get the maximum residue number in a PDB file.
@@ -46,7 +54,7 @@ def get_max_residue(pdb_file):
     return np.max(res_list)
 
 
-def pdb_to_cmap(pdb_file):
+def pdb_to_cmap_old(pdb_file):
     """
     Convert a PDB file to a contact map.
     :param pdb_file: (String) name of the PDB file
@@ -70,6 +78,46 @@ def pdb_to_cmap(pdb_file):
             cmap_matrix[partner_id][root_id] = 1
 
     return cmap_matrix
+############################################################
+
+
+def calc_dist_matrix(chain_one, chain_two):
+    """
+    Return a matrix of C-alpha distances between two protein chains.
+    :param chain_one: first chain
+    :param chain_two: second chain
+    :return: (torch.tensor) distance matrix
+    """
+
+    # Extract C-alpha coordinates
+    coords_one = np.array([residue["CA"].coord for residue in chain_one])
+    coords_two = np.array([residue["CA"].coord for residue in chain_two])
+
+    # Calculate pairwise distances using scipy.spatial.distance.cdist
+    dist_matrix = distance.cdist(coords_one, coords_two, 'euclidean')
+
+    # Convert the distance matrix to a PyTorch tensor
+    answer = torch.tensor(dist_matrix, dtype=torch.float32)
+
+    return answer
+
+
+def pdb_to_cmap(id, pdb_file, threshold=8, chain="A"):
+    """
+    Convert a PDB file to a contact map. The contact map is a matrix
+    such that element ij is 1 if the C-alpha distance between residues i and j
+    is less than the threshold, and 0 otherwise.
+    :param id: (string) ID of the protein structure
+    :param pdb_file: (String) path to the PDB file
+    :param threshold: (int) threshold distance for contacts
+    :param chain: (string) name of the chain to consider
+    :return: (torch.tensor) contact map
+    """
+    structure = Bio.PDB.PDBParser().get_structure(id, pdb_file)
+    model = structure[0]
+    dist_matrix = calc_dist_matrix(model[chain], model[chain])
+    contact_map = dist_matrix < threshold
+    return contact_map.to(torch.uint8)
 
 
 def plot_contact_map(contact_map, ax, title=""):
@@ -93,8 +141,13 @@ if __name__ == "__main__":
     #pdb_dir = "PDB_database"
     #pdb_dir = "../../data/swissprot_pdb_v4"
     dataloader = prepare_dataloaders(pdb_dir)
+    #i = 0
     for cmap, pdb_file in tqdm.tqdm(dataloader, total=len(dataloader)):
-        # fig, ax = plt.subplots()
-        # plot_contact_map(cmap[0], ax, title=str(pdb_file[0]))
-        # plt.show()
+        """
+        if i  < 10:
+            fig, ax = plt.subplots()
+            plot_contact_map(cmap[0], ax, title=str(pdb_file[0]))
+            plt.show()
+        """
+        #i += 1
         pass
