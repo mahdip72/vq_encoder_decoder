25d28845dae8a83b5feb66eef4a9400eb7f08dcd
mahdi <mahdipoormirzaie@gmail.com>
2024-05-23 21:11:56 -0500

    feat: add dataloader to test the supermodel in the main block

--- model/gvp_vqvae.py
+++ model/gvp_vqvae.py
@@ -1,16 +1,15 @@
 import torch.nn as nn
 from vector_quantize_pytorch import VectorQuantize
-from model.gvp import GVPEncoder
+from data.data import custom_collate, ProteinGraphDataset
+from gvp.models import GVPEncoder
+from torch.utils.data import DataLoader
 
 
-class LinearVQVAE(nn.Module):
+class VQVAE(nn.Module):
     def __init__(self, input_dim, latent_dim, num_embeddings, commitment_cost):
-        super(LinearVQVAE, self).__init__()
+        super(VQVAE, self).__init__()
 
-        self.encoder = nn.Linear(input_dim, latent_dim)
-        nn.init.xavier_uniform_(self.encoder.weight)
-
-        # self.encoder = LinearEncoder(input_dim, hidden_dim, latent_dim)
+        self.encoder = nn.Conv1d(input_dim, latent_dim, 1)
 
         self.vector_quantizer = VectorQuantize(
             dim=latent_dim,
@@ -18,10 +17,7 @@ class LinearVQVAE(nn.Module):
             decay=0.8,
             commitment_weight=1.0
         )
-        self.decoder = nn.Linear(latent_dim, input_dim)
-        nn.init.xavier_uniform_(self.decoder.weight)
-
-        # self.decoder = LinearDecoder(latent_dim, hidden_dim, input_dim)
+        self.decoder = nn.Conv1d(latent_dim, input_dim, 1)
 
     def forward(self, x, return_vq_only=False):
         x = self.encoder(x)
@@ -55,14 +51,15 @@ def prepare_models(configs):
     :return: (object) model
     """
     gvp = GVPEncoder(configs=configs)
-    vqvae = LinearVQVAE(
-        input_dim=configs.model.vector_quantization.input_dim,
+    vqvae = VQVAE(
+        input_dim=gvp.backbone,
         latent_dim=configs.model.vector_quantization.latent_dim,
         num_embeddings=configs.model.vector_quantization.codebook_size,
         commitment_cost=configs.model.vector_quantization.alpha
     )
+    gvp_vqvae = GVPVQVAE(gvp, vqvae, configs)
 
-    return model
+    return gvp_vqvae
 
 
 if __name__ == '__main__':
@@ -75,6 +72,26 @@ if __name__ == '__main__':
         config_file = yaml.full_load(file)
 
     main_configs = load_configs(config_file)
-    model = prepare_models(main_configs)
-    print(model)
+    test_model = prepare_models(main_configs)
+    print(test_model)
     print("Model loaded successfully!")
+
+    dataset = ProteinGraphDataset(config_file.train_settings.data_path,
+                                  seq_mode=config_file.model.struct_encoder.use_seq.seq_embed_mode,
+                                  use_rotary_embeddings=config_file.model.struct_encoder.use_rotary_embeddings,
+                                  use_foldseek=config_file.model.struct_encoder.use_foldseek,
+                                  use_foldseek_vector=config_file.model.struct_encoder.use_foldseek_vector,
+                                  top_k=config_file.model.struct_encoder.top_k,
+                                  num_rbf=config_file.model.struct_encoder.num_rbf,
+                                  num_positional_embeddings=config_file.model.struct_encoder.num_positional_embeddings)
+
+    val_loader = DataLoader(dataset, batch_size=config_file.train_settings.batch_size, num_workers=0, pin_memory=True,
+                            collate_fn=custom_collate)
+    struct_embeddings = []
+    test_model.eval()
+    for batch in val_loader:
+        graph = batch["graph"].to('gpu:1')
+        features_struct, _ = test_model(graph=graph)
+        struct_embeddings.extend(features_struct.cpu().detach().numpy())
+        print(struct_embeddings)
+        break
