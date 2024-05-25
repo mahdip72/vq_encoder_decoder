b108c20c1de3b2af027614a3fa7cfdd539e294e6
mahdi <mahdipoormirzaie@gmail.com>
2024-05-24 19:40:28 -0500

    ref: create utils for model

--- model/model.py
+++ model/model.py
@@ -5,11 +5,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
 from vector_quantize_pytorch import VectorQuantize, LFQ
-# import gvp.models
 from torch_geometric.nn import radius, global_mean_pool, global_max_pool
 from data import *
 from torch_geometric.nn import MessagePassing, global_mean_pool, global_max_pool
 from torch_scatter import scatter_add
+from model.utils import print_trainable_parameters
 
 
 def tuple_sum(*args):
@@ -674,41 +674,6 @@ class GVPEncoder(nn.Module):  # embedding table can be tuned
         return graph_feature_embedding, residue_feature_embedding
 
 
-def get_nb_trainable_parameters(model):
-    r"""
-    Returns the number of trainable parameters and number of all parameters in the model.
-    """
-    trainable_params = 0
-    all_param = 0
-    for _, param in model.named_parameters():
-        num_params = param.numel()
-        # if using DS Zero 3 and the weights are initialized empty
-        if num_params == 0 and hasattr(param, "ds_numel"):
-            num_params = param.ds_numel
-
-        # Due to the design of 4bit linear layers from bitsandbytes
-        # one needs to multiply the number of parameters by 2 to get
-        # the correct number of parameters
-        if param.__class__.__name__ == "Params4bit":
-            num_params = num_params * 2
-
-        all_param += num_params
-        if param.requires_grad:
-            trainable_params += num_params
-
-    return trainable_params, all_param
-
-
-def print_trainable_parameters(model, logging, description=""):
-    """
-    Prints the number of trainable parameters in the model.
-    """
-    trainable_params, all_param = get_nb_trainable_parameters(model)
-    logging.info(
-        f"{description} trainable params: {trainable_params: ,} || all params: {all_param: ,} || trainable%: {100 * trainable_params / all_param}"
-    )
-
-
 class _VDropout(nn.Module):
     '''
     Vector channel dropout where the elements of each
@@ -808,6 +773,8 @@ def get_dummy_logger():
 if __name__ == '__main__':
     import yaml
     from utils import load_configs
+    from data.data import ProteinGraphDataset, custom_collate
+    from torch.utils.data import DataLoader
 
     logger, buffer = get_dummy_logger()
 
