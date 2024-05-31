a3a935af1d0f5fe369f0ec087c9b2a322a919853
mahdi <mahdipoormirzaie@gmail.com>
2024-05-30 20:43:15 -0500

    fix: number of workers

--- data/dataset.py
+++ data/dataset.py
@@ -657,13 +657,13 @@ def prepare_vqvae_dataloaders(logging, accelerator, configs):
 
     train_loader = DataLoader(train_dataset, batch_size=configs.train_settings.batch_size,
                               shuffle=configs.train_settings.shuffle,
-                              # num_workers=configs.train_settings.num_workers,
+                              num_workers=configs.train_settings.num_workers,
                               multiprocessing_context='spawn' if configs.train_settings.num_workers > 0 else None,
                               pin_memory=True)
 
     valid_loader = DataLoader(valid_dataset, batch_size=configs.valid_settings.batch_size,
                               shuffle=False,
-                              # num_workers=configs.valid_settings.num_workers,
+                              num_workers=configs.valid_settings.num_workers,
                               multiprocessing_context='spawn' if configs.train_settings.num_workers > 0 else None,
                               pin_memory=True)
 
