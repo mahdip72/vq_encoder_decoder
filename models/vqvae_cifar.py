014cef025fdc6b1371f434f724de02848906472a
jren0 <zidar2003@gmail.com>
2024-06-05 16:58:42 -0500

    feat: ResNet VQVAE for CIFAR (bad performance)

--- models/vqvae_cifar.py
+++ models/vqvae_cifar.py
@@ -149,10 +149,95 @@ class VQVAE(nn.Module):
         return x, indices, commit_loss
 
 
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.downsample = None
+        if in_channels != out_channels:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(
+                    in_channels,
+                    out_channels,
+                    kernel_size=(3, 3),
+                    padding='same',
+                    bias=False
+                ),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        identity = self.downsample(x) if self.downsample else x
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x + identity)
+
+        return x
+
+
+class VQVAEResNet(nn.Module):
+    """
+    A VQVAE model with residual CNNs for encoder and decoder
+    """
+    def __init__(self, dim, codebook_size, decay, commitment_weight, configs):
+        super().__init__()
+        self.d_model = dim
+        self.num_layers = configs.model.num_layers
+
+        # enc_layers = get_layers(True, self.num_layers, configs)
+        # enc_layers.append(ResidualBlock(dim, dim))
+
+        self.encoder_layers = nn.Sequential(
+            nn.Conv2d(3, 8, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(8),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            ResidualBlock(8, 16),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.vq_layer = VectorQuantize(
+            dim=self.d_model,
+            codebook_size=codebook_size,
+            decay=decay,
+            commitment_weight=commitment_weight,
+            accept_image_fmap=True
+        )
+
+        # dec_layers = get_layers(False, self.num_layers, configs)
+        # dec_layers.append(ResidualBlock(3, 3))
+
+        self.decoder_layers = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode='nearest'),
+            nn.Conv2d(16, 8, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(8),
+
+            nn.Upsample(scale_factor=2, mode='nearest'),
+            ResidualBlock(8,3),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.encoder_layers(x)
+        x, indices, commit_loss = self.vq_layer(x)
+        x = self.decoder_layers(x)
+        return x, indices, commit_loss
+
+
 def prepare_models(configs, logger, accelerator):
     # from torchsummary import summary
 
-    vqvae = VQVAE(
+    vqvae = VQVAEResNet(
         dim=configs.model.vector_quantization.dim,
         codebook_size=configs.model.vector_quantization.codebook_size,
         decay=configs.model.vector_quantization.decay,
