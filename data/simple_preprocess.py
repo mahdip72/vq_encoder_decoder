97307f436febadf53346159eb389a004819194c0
mahdi <mahdipoormirzaie@gmail.com>
2024-06-06 18:22:58 -0500

    fix: multi chains and some bugs
    Fix problems exist in the experimental pdb files.

--- data/simple_preprocess.py
+++ data/simple_preprocess.py
@@ -2,9 +2,11 @@ import argparse
 import os
 from tqdm import tqdm
 from Bio.PDB import PDBParser
+from Bio.PDB.Polypeptide import PPBuilder
 import math
 import h5py
 from concurrent.futures import as_completed, ProcessPoolExecutor
+from multiprocessing import Manager
 
 
 def write_h5_file(file_path, pad_seq, n_ca_c_o_coord, plddt_scores):
@@ -15,15 +17,17 @@ def write_h5_file(file_path, pad_seq, n_ca_c_o_coord, plddt_scores):
 
 
 def extract_coordinates_plddt(chain, max_amino_acid_index, max_len):
-    """Returns a list of C-alpha coordinates for a chain"""
+    """Returns a list of N, C-alpha, C and O (backbone atoms) coordinates for a chain"""
     pos = []
     plddt_scores = []
     for row, residue in enumerate(chain):
         if row == max_amino_acid_index:
             break
         pos_N_CA_C_O = []
-        plddt_scores.append(residue["CA"].get_bfactor())
-        plddt_scores.append(0)
+        try:
+            plddt_scores.append(residue["CA"].get_bfactor())
+        except KeyError:
+            plddt_scores.append(0)
 
         for key in ['N', 'CA', 'C', 'O']:
             if key in residue:
@@ -37,25 +41,45 @@ def extract_coordinates_plddt(chain, max_amino_acid_index, max_len):
     return pos, plddt_scores
 
 
-def preprocess_file(file_path, max_len, save_path, dictn):
-    parser = PDBParser()  # This is from Bio.PDB
+def check_single_protein_chain(structure):
+    ppb = PPBuilder()
+    chains = [chain for model in structure for chain in model]
+    sequences = [pp.get_sequence() for chain in chains for pp in ppb.build_peptides(chain)]
+    return len(sequences) == 1, chains[0].id if sequences else None
+
+
+def preprocess_file(file_path, max_len, save_path, dictn, report_dict):
+    if os.path.basename(file_path) == '1H7M.pdb':
+        pass
+
+    parser = PDBParser(QUIET=True)  # This is from Bio.PDB
     structure = parser.get_structure('protein', file_path)
+
+    # Check for a single protein chain
+    single_chain, chain_id = check_single_protein_chain(structure)
+    if not single_chain:
+        report_dict['protein_complex'] += 1
+        return
+    if chain_id != 'A':
+        report_dict['no_chain_id_a'] += 1
+
     model = structure[0]
+    chain = model[chain_id]
+
     sequence = []
-    chain = model['A']
     protein_seq = ''
     max_amino_acid_index = 0
     for residue_index, residue in enumerate(chain):
+        if residue_index == 95:
+            pass
         if residue.id[0] == ' ' and residue.resname in dictn:
             sequence.append(residue.resname)
             amino_acid = [dictn[triple] for triple in sequence]
             protein_seq = "".join(amino_acid)
             max_amino_acid_index = residue_index
         elif residue.id[0] != ' ':
-            # print(f"TER line encountered at residue index {residue_index}. Breaking the loop.")
             break
         else:
-            # print(f"Skipping non-standard residue {residue.resname} at position {residue_index}")
             pass
 
     n_ca_c_o_coord, plddt_scores = extract_coordinates_plddt(chain, max_amino_acid_index, max_len)
@@ -86,15 +110,17 @@ def main():
              'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W',
              'ALA': 'A', 'VAL': 'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'}
 
-    with ProcessPoolExecutor(max_workers=args.max_workers) as executor:
-        futures = {executor.submit(preprocess_file, file_path, args.max_len, args.save_path, dictn): file_path for
-                   file_path in data_path}
-        for future in tqdm(as_completed(futures), total=len(futures), desc="Processing files"):
-            file_path = futures[future]
-            try:
-                future.result()
-            except Exception as exc:
-                print(f"An error occurred while processing {file_path}: {exc} {type(exc)}")
+    with Manager() as manager:
+        report_dict = manager.dict({'protein_complex': 0, 'no_chain_id_a': 0})
+        with ProcessPoolExecutor(max_workers=args.max_workers) as executor:
+            futures = {executor.submit(preprocess_file, file_path, args.max_len, args.save_path, dictn, report_dict): file_path for file_path in data_path}
+            for future in tqdm(as_completed(futures), total=len(futures), desc="Processing files"):
+                file_path = futures[future]
+                try:
+                    future.result()
+                except Exception as exc:
+                    print(f"An error occurred while processing {file_path}: {exc} {type(exc)}")
+        print(dict(report_dict))
 
 
 if __name__ == '__main__':
