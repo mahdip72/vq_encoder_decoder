869af3da09537b0343f9f598a2d5fa29a86a6e18
jren0 <zidar2003@gmail.com>
2024-06-28 09:38:12 -0500

    feat: calculate MAE for MDS-reconstructed coordinates

--- test_distance_map_mds.py
+++ test_distance_map_mds.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import torch
 from torch.utils.data import DataLoader
+from torchmetrics import MeanAbsoluteError
 from tqdm import tqdm
 import yaml
 from utils.utils import load_configs
@@ -13,7 +14,7 @@ with open(config_path) as file:
     config_file = yaml.full_load(file)
 configs = load_configs(config_file)
 
-configs.train_settings.data_path = "/home/renjz/data/swissprot_1024_h5"
+configs.train_settings.data_path = "/home/renjz/data/cath_4_3_0"
 
 dataset = DistanceMapVQVAEDataset(configs.train_settings.data_path, train_mode=True, rotate_randomly=False,
                                         configs=configs)
@@ -23,11 +24,23 @@ dataloader = DataLoader(dataset, batch_size=configs.train_settings.batch_size,
                           num_workers=configs.train_settings.num_workers,
                           pin_memory=False)
 
-print(dataset[0])
+mae = MeanAbsoluteError()
 
-for data in dataloader:
-    distance_map = data["input_distance_map"]
-    print(distance_map.size())
-    predicted_coords = batch_distance_map_to_coordinates(distance_map)
+# Get batches of distance maps from h5 files
+progress_bar = tqdm(dataloader)
+for data in progress_bar:
 
-    # TODO: Calculate MAE
+    distance_map = data["input_distance_map"][:,0,:,:]
+    target_coords = data["target_coords"]
+
+    # Reconstruct coordinates from distance map using MDS
+    rec_coords = batch_distance_map_to_coordinates(distance_map)
+
+    # Calculate mean absolute error between reconstructed and target coordinates
+    mae.update(rec_coords, target_coords)
+    current_mae = mae.compute().item()
+
+    progress_bar.set_postfix({"mae": current_mae})
+
+overall_mae = mae.compute().item()
+print("Overall MAE:", overall_mae)
