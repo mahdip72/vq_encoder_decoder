4d658b06b2140e11e628a6832c319c5dba957a80
jren0 <zidar2003@gmail.com>
2024-07-01 15:36:56 -0500

    feat: Procrustes and distance map denormalization for MDS

--- test_distance_map_mds.py
+++ test_distance_map_mds.py
@@ -1,11 +1,14 @@
 from pathlib import Path
+from scipy.spatial import procrustes
 import torch
 from torch.utils.data import DataLoader
 from torchmetrics import MeanAbsoluteError
 from tqdm import tqdm
 import yaml
+
 from utils.utils import load_configs
 from data.dataset import DistanceMapVQVAEDataset
+from data.normalizer import Protein3DProcessing
 from utils.metrics import batch_distance_map_to_coordinates
 
 
@@ -57,24 +60,42 @@ dataloader = DataLoader(dataset, batch_size=configs.train_settings.batch_size,
                           num_workers=configs.train_settings.num_workers,
                           pin_memory=False)
 
+# Prepare the normalizer for denormalization
+processor = Protein3DProcessing()
+processor.load_normalizer(configs.normalizer_path)
+
+# Prepare processor for normalizing coordinates
+# coords_processor = Protein3DProcessing()
+# coords_processor.load_normalizer("data/normalizer.pkl")
+
 mae = MeanAbsoluteError()
 
 # Get batches of distance maps from h5 files
 progress_bar = tqdm(dataloader)
 for data in progress_bar:
 
-    distance_map = data["input_distance_map"][:,0,:,:]
+    distance_maps = data["input_distance_map"][:,0,:,:]
     target_coords = data["target_coords"]
 
     mds_args = {'n_components': 3, 'dissimilarity': 'precomputed', 'random_state': 42,
-                'n_init': 4, 'max_iter': 300, 'eps': 1e-5, 'n_jobs': -1}
+                'n_init': 10, 'max_iter': 300, 'eps': 1e-9, 'n_jobs': -1}
+
+    # Denormalize distance maps
+    for i in range(len(distance_maps)):
+        distance_maps[i] = processor.denormalize_distance_map(distance_maps[i])
 
     # Reconstruct coordinates from distance map using MDS
-    rec_coords = batch_distance_map_to_coordinates(distance_map, n_components=mds_args['n_components'],
+    rec_coords = batch_distance_map_to_coordinates(distance_maps, n_components=mds_args['n_components'],
                                                    dissimilarity=mds_args['dissimilarity'],
                                                    random_state=mds_args['random_state'],
                                                    n_init=mds_args['n_init'], max_iter=mds_args['max_iter'],
                                                    eps=mds_args['eps'], n_jobs=mds_args['n_jobs'])
+
+    # Perform Procrustes analysis on coordinates
+    for i in range(len(target_coords)):
+        new_target, new_rec, disparity = procrustes(target_coords[i], rec_coords[i])
+        target_coords[i] = torch.tensor(new_target)
+        rec_coords[i] = torch.tensor(new_rec)
 
     # Calculate mean absolute error between reconstructed and target coordinates
     mae.update(rec_coords, target_coords)
@@ -83,13 +104,14 @@ for data in progress_bar:
     progress_bar.set_postfix({"mae": current_mae})
 
     """
+    print("After Procrustes")
     print(target_coords[0][0:3])
     print(rec_coords[0][0:3])
-    print(target_coords[0][1]-target_coords[0][0])
-    print(rec_coords[0][1]-rec_coords[0][0])
+    #print(calc_dist(target_coords[0][0], target_coords[0][1]))
+    #print(calc_dist(rec_coords[0][0], rec_coords[0][1]))
 
     print('\n')
-    print(distance_map[0][0][0:2])
+    #print(distance_maps[0][0][0:3])
     exit()
     """
 
