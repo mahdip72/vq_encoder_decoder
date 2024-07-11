82887ec8b279aab5f011a9ffbe9084482d15d0e7
renjz <zidar2003@gmail.com>
2024-07-11 11:20:14 -0500

    feat: SimpleDistanceMapDataset for MDS testing

--- test_distance_map_mds.py
+++ test_distance_map_mds.py
@@ -1,12 +1,16 @@
 from pathlib import Path
 from scipy.spatial import procrustes
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from torchmetrics import MeanAbsoluteError
 from tqdm import tqdm
 import yaml
+import os
+import glob
 
 from utils.utils import load_configs
+from utils.utils import load_h5_file
+
 from data.dataset import DistanceMapVQVAEDataset
 from data.normalizer import Protein3DProcessing
 from utils.metrics import batch_distance_map_to_coordinates
@@ -45,75 +49,185 @@ def transform_rec_coords(rec_coords, target_coords):
     return new_coords
 
 
-config_path = "configs/config_distance_map_vqvae.yaml"
-with open(config_path) as file:
-    config_file = yaml.full_load(file)
-configs = load_configs(config_file)
-
-configs.train_settings.data_path = "/home/renjz/data/cath_4_3_0"
-
-dataset = DistanceMapVQVAEDataset(configs.train_settings.data_path, train_mode=True, rotate_randomly=False,
-                                        configs=configs)
-
-dataloader = DataLoader(dataset, batch_size=configs.train_settings.batch_size,
-                          shuffle=configs.train_settings.shuffle,
-                          num_workers=configs.train_settings.num_workers,
-                          pin_memory=False)
-
-# Prepare the normalizer for denormalization
-processor = Protein3DProcessing()
-processor.load_normalizer(configs.normalizer_path)
-
-# Prepare processor for normalizing coordinates
-# coords_processor = Protein3DProcessing()
-# coords_processor.load_normalizer("data/normalizer.pkl")
-
-mae = MeanAbsoluteError()
-
-# Get batches of distance maps from h5 files
-progress_bar = tqdm(dataloader)
-for data in progress_bar:
-
-    distance_maps = data["input_distance_map"][:,0,:,:]
-    target_coords = data["target_coords"]
-
-    mds_args = {'n_components': 3, 'dissimilarity': 'precomputed', 'random_state': 42,
-                'n_init': 4, 'max_iter': 200, 'eps': 1e-7, 'n_jobs': -1}
-
-    # Denormalize distance maps
-    for i in range(len(distance_maps)):
-        distance_maps[i] = processor.denormalize_distance_map(distance_maps[i])
-
-    # Reconstruct coordinates from distance map using MDS
-    rec_coords = batch_distance_map_to_coordinates(distance_maps, n_components=mds_args['n_components'],
-                                                   dissimilarity=mds_args['dissimilarity'],
-                                                   random_state=mds_args['random_state'],
-                                                   n_init=mds_args['n_init'], max_iter=mds_args['max_iter'],
-                                                   eps=mds_args['eps'], n_jobs=mds_args['n_jobs'])
-
-    # Perform Procrustes analysis on coordinates
-    for i in range(len(target_coords)):
-        new_target, new_rec, disparity = procrustes(target_coords[i], rec_coords[i])
-        target_coords[i] = torch.tensor(new_target)
-        rec_coords[i] = torch.tensor(new_rec)
-
-    # Calculate mean absolute error between reconstructed and target coordinates
-    mae.update(rec_coords, target_coords)
-    current_mae = mae.compute().item()
-
-    progress_bar.set_postfix({"mae": current_mae})
-
+class SimpleDistanceMapDataset(Dataset):
     """
-    print("After Procrustes")
-    print(target_coords[0][0:3])
-    print(rec_coords[0][0:3])
-    #print(calc_dist(target_coords[0][0], target_coords[0][1]))
-    #print(calc_dist(rec_coords[0][0], rec_coords[0][1]))
-
-    print('\n')
-    #print(distance_maps[0][0][0:3])
-    exit()
+    Simplified dataset for converting h5 files to distance map (no normalization)
     """
+    def __init__(self, data_path, configs):
+        """
+        :param data_path: (string or Path) path to directory of h5 files
+        :param configs: (Box) configurations
+        """
+        self.max_task_samples = configs.train_settings.max_task_samples
+        self.max_length = configs.model.max_length
+        self.data_path = str(data_path)
+        self.h5_samples = glob.glob(os.path.join(self.data_path, '*.h5'))[:self.max_task_samples]
 
-overall_mae = mae.compute().item()
-print("Overall MAE:", overall_mae)
+    def __len__(self):
+        return len(self.h5_samples)
+
+    @staticmethod
+    def handle_nan_coordinates(coords: torch.Tensor) -> torch.Tensor:
+        """
+        Replaces NaN values in the coordinates with the previous or next valid coordinate values.
+
+        Parameters:
+        -----------
+        coords : torch.Tensor
+            A tensor of shape (N, 4, 3) representing the coordinates of a protein structure.
+
+        Returns:
+        --------
+        torch.Tensor
+            The coordinates with NaN values replaced by the previous valid coordinate values.
+        """
+        # Flatten the coordinates for easier manipulation
+        original_shape = coords.shape
+        coords = coords.view(-1, 3)
+
+        # check if there are any NaN values in the coordinates
+        while torch.isnan(coords).any():
+            # Identify NaN values
+            nan_mask = torch.isnan(coords)
+
+            if not nan_mask.any():
+                return coords.view(original_shape)  # Return if there are no NaN values
+
+            # Iterate through coordinates and replace NaNs with the previous valid coordinate
+            for i in range(1, coords.shape[0]):
+                if nan_mask[i].any() and not torch.isnan(coords[i - 1]).any():
+                    coords[i] = coords[i - 1]
+
+            for i in range(0, coords.shape[0] - 1):
+                if nan_mask[i].any() and not torch.isnan(coords[i + 1]).any():
+                    coords[i] = coords[i + 1]
+
+        return coords.view(original_shape)
+
+    @staticmethod
+    def create_distance_map(coords):
+        """
+        Computes the pairwise distance map for CA coordinates using PyTorch.
+
+        Parameters:
+        ca_coords (torch.Tensor): A 2D tensor of shape (n, 3) containing the coordinates of CA atoms.
+
+        Returns:
+        torch.Tensor: A 2D tensor of shape (n, n) containing the pairwise distances.
+        """
+        diff = coords.unsqueeze(1) - coords.unsqueeze(0)
+        distance_map = torch.sqrt(torch.sum(diff ** 2, dim=-1))
+        return distance_map
+
+    def __getitem__(self, idx):
+
+        sample_path = self.h5_samples[idx]
+        sample = load_h5_file(sample_path)
+        basename = os.path.basename(sample_path)
+        pid = basename.split('.h5')[0].split('_')[0]
+        coords_list = sample[1].tolist()
+        coords_tensor = torch.Tensor(coords_list)
+
+        coords_tensor = coords_tensor[:self.max_length, ...]
+
+        coords_tensor = self.handle_nan_coordinates(coords_tensor)
+        coords_tensor = coords_tensor[..., 3:6].reshape(1, -1, 3)
+        distance_map = self.create_distance_map(coords_tensor.squeeze(0))
+
+        # expand the first dimension of distance maps
+        distance_map = distance_map.unsqueeze(0)
+
+        return {'pid': pid, 'coords': coords_tensor.squeeze(0), 'distance_map': distance_map}
+
+
+def prepare_dataloaders(configs):
+    """
+    Prepare distance map dataloaders.
+    :param configs: configurations for contact map
+    :return: train dataloader
+    """
+    data_path = configs.train_settings.data_path
+
+    # dataset = DistanceMapVQVAEDataset(data_path, train_mode=True, rotate_randomly=False, configs=configs)
+    dataset = SimpleDistanceMapDataset(data_path, configs)
+
+    dataloader = DataLoader(dataset, batch_size=configs.train_settings.batch_size,
+                              shuffle=configs.train_settings.shuffle,
+                              num_workers=configs.train_settings.num_workers,
+                              pin_memory=False)
+
+    return dataloader
+
+
+if __name__ == "__main__":
+
+    config_path = "configs/config_distance_map_vqvae.yaml"
+    with open(config_path) as file:
+        config_file = yaml.full_load(file)
+    main_configs = load_configs(config_file)
+
+    main_configs.train_settings.data_path = "/home/renjz/data/cath_4_3_0"
+
+    # # Prepare the normalizer for denormalization
+    # processor = Protein3DProcessing()
+    # processor.load_normalizer(main_configs.normalizer_path)
+    #
+    # # Prepare processor for normalizing coordinates
+    # # coords_processor = Protein3DProcessing()
+    # # coords_processor.load_normalizer("data/normalizer.pkl")
+
+    dmap_dataloader = prepare_dataloaders(main_configs)
+
+    mae = MeanAbsoluteError()
+
+    # Get batches of distance maps from h5 files
+    progress_bar = tqdm(dmap_dataloader)
+    for data in progress_bar:
+
+        distance_maps = data["distance_map"][:, 0, :, :]
+        target_coords = data["coords"]
+
+        print(target_coords)
+        print(target_coords.size())
+        print(distance_maps.size())
+
+        mds_args = {'n_components': 3, 'dissimilarity': 'precomputed', 'random_state': 42,
+                    'n_init': 4, 'max_iter': 300, 'eps': 1e-7, 'n_jobs': -1}
+
+        # # Denormalize distance maps
+        # for i in range(len(distance_maps)):
+        #     distance_maps[i] = processor.denormalize_distance_map(distance_maps[i])
+
+        # Reconstruct coordinates from distance map using MDS
+        rec_coords = batch_distance_map_to_coordinates(distance_maps, n_components=mds_args['n_components'],
+                                                       dissimilarity=mds_args['dissimilarity'],
+                                                       random_state=mds_args['random_state'],
+                                                       n_init=mds_args['n_init'], max_iter=mds_args['max_iter'],
+                                                       eps=mds_args['eps'], n_jobs=mds_args['n_jobs'])
+
+        # Perform Procrustes analysis on coordinates
+        for i in range(len(target_coords)):
+            new_target, new_rec, disparity = procrustes(target_coords[i], rec_coords[i])
+            target_coords[i] = torch.tensor(new_target)
+            rec_coords[i] = torch.tensor(new_rec)
+
+        # Calculate mean absolute error between reconstructed and target coordinates
+        mae.update(rec_coords, target_coords)
+        current_mae = mae.compute().item()
+
+        progress_bar.set_postfix({"mae": current_mae})
+
+        """
+        print("After Procrustes")
+        print(target_coords[0][0:3])
+        print(rec_coords[0][0:3])
+        #print(calc_dist(target_coords[0][0], target_coords[0][1]))
+        #print(calc_dist(rec_coords[0][0], rec_coords[0][1]))
+    
+        print('\n')
+        #print(distance_maps[0][0][0:3])
+        exit()
+        """
+
+    overall_mae = mae.compute().item()
+    print("Overall MAE:", overall_mae)
