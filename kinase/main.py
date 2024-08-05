5fdb9cf78f44b13b8b9b1987cd31a0ae17cb7515
Jarett Ren <zidar2003@gmail.com>
2024-08-05 09:30:48 -0500

    feat: get unique kinase sequences

--- kinase/main.py
+++ kinase/main.py
@@ -1,10 +1,11 @@
 from transformers import EsmTokenizer, EsmModel
 import torch
+import pandas as pd
 
 
 def get_protein_embedding(sequence):
     # Load the tokenizer and model
-    model_name = "facebook/esm2_t33_650M_UR50D"  # Example model, change as needed
+    model_name = "facebook/esm2_t12_35M_UR50D"  # Example model, change as needed
     tokenizer = EsmTokenizer.from_pretrained(model_name)
     model = EsmModel.from_pretrained(model_name)
 
@@ -19,8 +20,29 @@ def get_protein_embedding(sequence):
     return embeddings
 
 
+def get_unique_kinases(kinase_df):
+    """
+    Get the unique kinase sequences from a dataset.
+    :param kinase_df: DataFrame of kinase data
+    :return kinase_dict: dictionary of unique name:sequence kinase pairs
+    """
+    # Drop duplicates to get unique kinase samples
+    unique_df = kinase_df.drop_duplicates(subset='kinase')
+
+    # Create a dictionary with kinase as keys and kinase_sequence as values
+    kinase_dict = unique_df.set_index('kinase')['kinase_sequence'].to_dict()
+
+    return kinase_dict
+
+
 if __name__ == '__main__':
     # Example usage
     protein_sequence = "MKTAYIAKQRQISFVKSHFSRQLEERLGLIEVQAYVLSMSPARGCVTRDCRVCTRVYADRTKFGINPQTFRYYTDRVRFDG"  # Replace with your sequence
     embedding = get_protein_embedding(protein_sequence)
     print(embedding.shape)
+
+    data_path = "../../data/valid_filtered.csv"
+    kinase_df = pd.read_csv(data_path)
+
+    kinase_dict = get_unique_kinases(kinase_df)
+    print(kinase_dict.keys())
