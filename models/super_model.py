4830b4fca2cdbc459274253e92c02cc893c0587d
mahdi <mahdipoormirzaie@gmail.com>
2024-11-24 23:17:59 -0600

    ref: make the model components modular

---
+++ models/super_model.py
@@ -0,0 +1,115 @@
+import torch.nn as nn
+import torch
+import torch_geometric
+from models.encoders import GVPTransformerEncoderWrapper
+from models.vqvae import VQVAETransformer
+from models.decoders import GCPNetDecoder, GeometricDecoder
+from gcpnet.models.gcpnet import GCPNetModel
+# from gcpnet.models.vqvae import CategoricalMixture, PairwisePredictionHead, RegressionHead
+# from gcpnet.utils.structure.predicted_aligned_error import compute_predicted_aligned_error, compute_tm
+from utils.utils import print_trainable_parameters
+from models.utils import merge_features, separate_features
+
+
+class SuperModel(nn.Module):
+    def __init__(self, encoder, vqvae, decoder, configs):
+        super(SuperModel, self).__init__()
+        self.encoder = encoder
+        self.vqvae = vqvae
+        self.decoder = decoder
+
+        self.configs = configs
+        self.max_length = configs.model.max_length
+
+
+    def forward(self, batch):
+        x = self.encoder(batch, output_logits=False)
+
+        # x = separate_features(x, batch['graph'].batch)
+        x, mask, batch_indices, x_slice_indices = merge_features(x, self.max_length)
+
+        x, indices, commit_loss = self.vqvae(x, mask)
+
+        x = self.decoder(x, mask, batch_indices, x_slice_indices)
+
+        # return x, indices, commit_loss
+        return x, torch.Tensor([0]).to(x[0].device), torch.Tensor([0]).to(x[0].device)
+
+
+def prepare_model_vqvae(configs, logger, accelerator, **kwargs):
+
+    if configs.model.encoder == "gcpnet":
+        encoder = GCPNetModel(module_cfg=configs.model.struct_encoder.module_cfg,
+                              model_cfg=configs.model.struct_encoder.model_cfg,
+                              layer_cfg=configs.model.struct_encoder.layer_cfg,
+                              configs=configs)
+    elif configs.model.encoder == "gvp_transformer":
+        encoder = GVPTransformerEncoderWrapper(output_logits=False, finetune=True)
+    else:
+        raise ValueError("Invalid encoder model specified!")
+
+    vqvae = VQVAETransformer(
+        latent_dim=configs.model.vqvae.vector_quantization.dim,
+        codebook_size=configs.model.vqvae.vector_quantization.codebook_size,
+        decay=configs.model.vqvae.vector_quantization.decay,
+        configs=configs
+    )
+
+    if configs.model.decoder == "gcpnet":
+        decoder = GCPNetDecoder(configs, decoder_configs=kwargs["decoder_configs"])
+    elif configs.model.decoder == "geometric_decoder":
+        decoder = GeometricDecoder(configs, decoder_configs=kwargs["decoder_configs"])
+    else:
+        raise ValueError("Invalid decoder model specified!")
+
+    vqvae = SuperModel(encoder, vqvae, decoder, configs)
+
+    vqvae = nn.SyncBatchNorm.convert_sync_batchnorm(vqvae)
+
+    if accelerator.is_main_process:
+        print_trainable_parameters(vqvae, logger, 'SuperVQVAE')
+
+    return vqvae
+
+
+if __name__ == '__main__':
+    import yaml
+    import tqdm
+    from utils.utils import load_configs, get_dummy_logger
+    from torch.utils.data import DataLoader
+    from accelerate import Accelerator
+    from data.dataset import custom_collate, GCPNetDataset
+
+    config_path = "../configs/config_gcpnet.yaml"
+
+    with open(config_path) as file:
+        config_file = yaml.full_load(file)
+
+    test_configs = load_configs(config_file)
+
+    test_logger = get_dummy_logger()
+    test_accelerator = Accelerator()
+
+    test_model = prepare_model_vqvae(test_configs, test_logger, test_accelerator)
+
+    print("Model loaded successfully!")
+
+    dataset = GCPNetDataset(test_configs.train_settings.data_path,
+                            seq_mode=test_configs.model.struct_encoder.use_seq.seq_embed_mode,
+                            use_rotary_embeddings=test_configs.model.struct_encoder.use_rotary_embeddings,
+                            use_foldseek=test_configs.model.struct_encoder.use_foldseek,
+                            use_foldseek_vector=test_configs.model.struct_encoder.use_foldseek_vector,
+                            top_k=test_configs.model.struct_encoder.top_k,
+                            num_rbf=test_configs.model.struct_encoder.num_rbf,
+                            num_positional_embeddings=test_configs.model.struct_encoder.num_positional_embeddings,
+                            configs=test_configs)
+
+    test_loader = DataLoader(dataset, batch_size=test_configs.train_settings.batch_size, num_workers=0, pin_memory=True,
+                             collate_fn=custom_collate)
+    struct_embeddings = []
+    test_model.eval()
+    for batch in tqdm.tqdm(test_loader, total=len(test_loader)):
+        graph = batch["graph"]
+        output, _, _ = test_model(batch)
+        print(output.shape)
+        break
