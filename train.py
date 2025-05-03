b282412c26a71394f5b1b42cf82022bf24daab03
Alex Morehead <alex.morehead@gmail.com>
2025-05-03 13:20:45 -0500

    Profile memory usage

--- train.py
+++ train.py
@@ -461,6 +461,7 @@ def main(dict_config, config_file_path):
             schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=1),
             on_trace_ready=torch.profiler.tensorboard_trace_handler(configs.train_settings.profiler_log_dir),
             record_shapes=True,
+            profile_memory=True,
             with_stack=True,
         )
         prof.start()
