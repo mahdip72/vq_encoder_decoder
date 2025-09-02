87ef1cf71ef8101eecfbab78d6a4955949087dd2
mahdi <mahdipoormirzaie@gmail.com>
2025-09-02 17:29:31 -0500

    fix: adjust upper and lower thresholds in coefficient adjustment function

--- utils/custom_losses.py
+++ utils/custom_losses.py
@@ -31,7 +31,7 @@ def compute_grad_norm(loss, parameters, norm_type=2):
 
 
 def adjust_coeff_by_grad(coeff, grad_norm, decrease_factor=0.9, increase_factor=1.1,
-                        upper_thresh=1.0, lower_thresh=0.1):
+                        upper_thresh=0.5, lower_thresh=0.05):
     """
     Adjust a coefficient based on gradient norm magnitude.
 
