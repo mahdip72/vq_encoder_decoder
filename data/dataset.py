8c030d77621d0f01893ae9e2ed138ba7d3dcffb5
mahdi <mahdipoormirzaie@gmail.com>
2025-09-25 14:39:55 -0500

    fix: add mapping for letter 'Z' in letter-to-number conversion

--- data/dataset.py
+++ data/dataset.py
@@ -361,7 +361,7 @@ class GCPNetDataset(Dataset):
         self.letter_to_num = {'C': 4, 'D': 3, 'S': 15, 'Q': 5, 'K': 11, 'I': 9,
                               'P': 14, 'T': 16, 'F': 13, 'A': 0, 'G': 7, 'H': 8,
                               'E': 6, 'L': 10, 'R': 1, 'W': 17, 'V': 19,
-                              'N': 2, 'Y': 18, 'M': 12, 'X': 20}
+                              'N': 2, 'Y': 18, 'M': 12, 'X': 20, 'Z': 20}
         self.num_to_letter = {v: k for k, v in self.letter_to_num.items()}
 
         self.max_length = kwargs['configs'].model.max_length
